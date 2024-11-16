3a26605715f7e83e2f593723d90c8d928dbc7952
Jan-Ru Muller <janru.muller@gmail.com>
2024-11-16 13:15:32 +0100

    corrected +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,24 @@
 <script>
   let greeting = 'Welcome to SvelteKit JRM';
 </script>
+
+<main>
+  <h1>{greeting}</h1>
+  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>
